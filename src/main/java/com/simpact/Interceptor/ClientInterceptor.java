e58a09cb407d7c4ed2ddcecdfbe154d6d30f00da
SungWook <syyoo991@naver.com>
2017-07-11 18:45:26 +0900

    유성욱 세션해제시켜놓은부분제거 클라이언트 인터셉터처리 (미완)
    --수정--
    src\main\webapp\WEB-INF\views\client\counsel\list.jsp
    src\main\webapp\WEB-INF\spring\appServlet\servlet-context.xml
    
    --추가--
    src\main\java\com\simpact\Interceptor\ClientInterceptor.java

---
+++ src/main/java/com/simpact/Interceptor/ClientInterceptor.java
@@ -0,0 +1,24 @@
+package com.simpact.Interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import com.simpact.domain.MemberVO;
+
+public class ClientInterceptor extends HandlerInterceptorAdapter{
+	
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		
+		MemberVO vo = (MemberVO) request.getSession().getAttribute("clientMemberVO");
+		if(vo == null){
+			response.sendRedirect("/lte");	//초기화면으로
+			return false;
+		}
+		return super.preHandle(request, response, handler);
+	}
+
+}
--- src/main/webapp/WEB-INF/views/client/counsel/list.jsp
+++ src/main/webapp/WEB-INF/views/client/counsel/list.jsp
@@ -9,7 +9,6 @@
 <%@include file="../include/header.jsp" %>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
-<%@ page session="false"%>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 <!-- Main content -->
